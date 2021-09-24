a23c1c841d005c9782d136b1027cc6af3104c1b7
Anoop Kumar <89188571+Anoop-Kumar-31@users.noreply.github.com>
2021-09-25 00:21:52 +0530

    The Number Guess Game [4 digit number]
    In this the first person set the number and second person try to guess the number. The computer will tell how many numbers are common in the entered and set number, and also tell how many are in right position in the entered number. 
    (/~_~)/ HAVE fun

---
+++ TheNumberGuessGame.py
@@ -0,0 +1,40 @@
+def guess(d,l):
+      gue=int(input("Player two: Enter Your 4 digit guess: "))
+      dic=dict()
+      ll=[]
+      while(gue>0):
+            a=gue%10
+            gue//=10
+            ll.append(a)
+      ll.reverse()
+      for i in range(len(ll)):
+            dic[i]=ll[i]
+      y=0
+      z=0
+      for i in range(len(ll)):
+            if ll[i] in l:
+                  y+=1
+                  if(ll[i]==l[i]):
+                        z+=1
+      if(y==len(ll) and z==len(ll)):
+            print("Congrats right guess")
+      else:
+            print("Right number:",y)
+            print("Right location:",z)
+            if(input("Want to try again[y/s]:")=='y'):
+                  guess(d,l)
+            else:
+                  print("Better Luck next time")
+origin=int(input('Player one: Enter your 4 digit number: '))
+copy=origin
+d=dict()
+l=[]
+while(copy>0):
+      a=copy%10
+      copy//=10
+      l.append(a)
+l.reverse()
+for i in range(len(l)):
+      d[i]=l[i]
+guess(d,l)
+
